7d45dcfe56fa6af0627e874d896cb0c2fda0f69b
Joe Varghese <joevarghese53@gmail.com>
2026-04-11 20:27:30 +0530

    fixed sizes of category images

--- components/shopByCategory/categoryCard/categoryCard.tsx
+++ components/shopByCategory/categoryCard/categoryCard.tsx
@@ -16,6 +16,7 @@ export default function CategoryCard({
         src={image}
         alt={title}
         fill
+        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
         className="object-cover transition duration-700 group-hover:scale-110"
       />
 
