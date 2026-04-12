e6f72ebaa91bdcf3e8d0155486aa9e3d573d235f
Joe Varghese <joevarghese53@gmail.com>
2026-04-12 13:14:52 +0530

    made product card clickable

--- components/productCard/productCard.tsx
+++ components/productCard/productCard.tsx
@@ -5,27 +5,30 @@ import { Heart, Plus } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { ProductType } from "@/types/productType"
+import Link from "next/link"
 
 export default function ProductCard({ product }: { product: ProductType }) {
   return (
     <Card className="w-70 border-none">
       <CardContent className="p-0">
         {/* Product Image */}
-        <div className="relative w-full overflow-hidden rounded-xl bg-muted">
-          <Image
-            src={product.frontImage}
-            alt={product.name}
-            width={300}
-            height={300}
-            className="h-auto w-full object-cover"
-          />
+        <div className="relative w-full overflow-hidden rounded-xl bg-muted cursor-pointer">
+
+          <Link href={`/product/${product._id}`}>
+            <Image
+              src={product.frontImage}
+              alt={product.name}
+              width={300}
+              height={300}
+              className="h-auto w-full object-cover"
+            />
+          </Link>
 
           {/* Wishlist */}
-          <button className="absolute top-3 right-3 rounded-full bg-white p-2 shadow-sm transition hover:scale-110">
+          <button className="absolute top-3 right-3 cursor-pointer rounded-full bg-white p-2 shadow-sm transition hover:scale-110">
             <Heart className="h-4 w-4" />
           </button>
         </div>
-
         {/* Product Info */}
         <div className="mt-3 flex items-start justify-between">
           <div>
@@ -41,7 +44,7 @@ export default function ProductCard({ product }: { product: ProductType }) {
         {/* Add to Cart */}
         <Button
           variant="outline"
-          className="mt-3 flex w-full items-center gap-2"
+          className="mt-3 flex w-full cursor-pointer items-center gap-2"
         >
           <Plus size={16} />
           Add to Cart
